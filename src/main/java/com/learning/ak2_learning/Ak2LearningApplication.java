71c41a26ad22b75f9f613c1521ea2d8c3be30743
“akki-to” <yanach1n919@gmail.com>
2024-11-18 02:42:30 +0900

    first commit

---
+++ src/main/java/com/learning/ak2_learning/Ak2LearningApplication.java
@@ -0,0 +1,13 @@
+package com.learning.ak2_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ak2LearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ak2LearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/ak2_learning/Ak2LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.ak2_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ak2LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
